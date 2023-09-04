c2208abbac5b740593fb472c3f3e18b87683698b
AYUSH SINGLA <84726355+Ayush2106@users.noreply.github.com>
2023-09-04 21:49:15 +0530

    first

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Navbar from './src/components/Navbar/Navbar';
+// import Slide from './src/components/Slider/Slide';
+import Pickdrop from './src/components/pickdrop/Pickdrop';
+import Footer from './src/components/footer/Footer';
+import Slidee from './src/components/Slider/Slidee';
+import Pages from './src/components/pages/Pages';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello G</Text>
+      <Navbar/>
+      <Slidee/>
+<Pickdrop/>
+      <Footer/>
+ <Pages/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#391285',
+    paddingHorizontal:'5%',
+    paddingTop:'8%',
+    paddingBottom:'25%',
   },
 });
